1506509e273aa680da040e1ed5d78e941e8d42e4
jairorr <jairo.2aldair@gmail.com>
2023-01-05 19:59:38 -0500

    update location permission

--- app/src/main/java/com/example/customweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/customweatherapp/MainActivity.kt
@@ -9,74 +9,95 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.customweatherapp.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bottomNav:BottomNavigationView
+    private lateinit var bottomNav: BottomNavigationView
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     /*Default values lat and lon*/
-    private var latitude = -8.11599
-    private var longitude = -79.02998
+    private var latitude = -12.04318
+    private var longitude = -77.02824
 
-    private var requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
-        isGranted -> setRealLatLon(isGranted)
-    }
+    private var requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            setRealLatLon(isGranted)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-        initHomeFragment()
         bottomNav = binding.bottomNavigationView
 
-        bottomNav.setOnItemSelectedListener { menu->
-            when(menu.itemId){
-                R.id.home-> {
+        bottomNav.setOnItemSelectedListener { menu ->
+            when (menu.itemId) {
+                R.id.home -> {
                     initHomeFragment()
                     true
                 }
-                else->false
+                else -> false
             }
         }
     }
 
     private fun initHomeFragment() {
+        Log.d("getLocation", "Inicio de fragment home")
         val bundle = Bundle().apply {
-            putDouble("LATITUDE",latitude)
-            putDouble("LONGITUDE",longitude)
+            putDouble("LATITUDE", latitude)
+            putDouble("LONGITUDE", longitude)
         }
-        val homeFragment = HomeFragment().apply{ arguments = bundle }
-        supportFragmentManager.beginTransaction().replace(binding.fragmentReplacer.id,homeFragment).commit()
+        val homeFragment = HomeFragment().apply { arguments = bundle }
+        supportFragmentManager.beginTransaction().replace(binding.fragmentReplacer.id, homeFragment)
+            .commit()
     }
 
     private fun setRealLatLon(isLocationGranted: Boolean) {
-        when(isLocationGranted){
-            true ->{
+        when (isLocationGranted) {
+            true -> {
                 if (ActivityCompat.checkSelfPermission(
                         this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
                 ) {
-                    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
-                    fusedLocationClient.lastLocation.addOnCompleteListener {
-                        if(it.result.latitude != null){
-                            latitude = it.result.latitude
-                            longitude = it.result.longitude
+                    lifecycleScope.launch(Dispatchers.IO){
+                        fusedLocationClient =
+                            LocationServices.getFusedLocationProviderClient(this@MainActivity)
+                        fusedLocationClient.lastLocation.addOnCompleteListener {
+                            if (it.result != null) {
+                                latitude = it.result.latitude
+                                longitude = it.result.longitude
+                            }
+                            initHomeFragment()
                         }
                     }
                     return
                 }
             }
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)->{
-
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) -> {
+                Toast.makeText(
+                    this,
+                    "Al denegar el permiso no se puede acceder a su ubicacion actual",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return
             }
-            else->{
-                Toast.makeText(this,"Debe aceptar el permiso de ubicacion para obtener su ubicacion",Toast.LENGTH_SHORT).show()
+            else -> {
+                Toast.makeText(
+                    this,
+                    "Para activar el permiso de localizacion, tiene que ir a configuracion.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return
             }
         }
     }
